57d12f9b46cd9530f23a9a19f33e69ba7b564906
Yoann MOUGNIBAS <yoann@mougnibas.fr>
2024-04-16 23:07:58 +0200

    Customize default CLI project

--- PgsHelperCli/Package.swift
+++ PgsHelperCli/Package.swift
@@ -7,6 +7,7 @@ let package = Package(
     name: "PgsHelperCli",
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
+        .package(path: "../PgsHelperCore")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -15,6 +16,7 @@ let package = Package(
             name: "PgsHelperCli",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "PgsHelperCore",  package: "PgsHelperCore")
             ]
         ),
     ]
--- PgsHelperCli/Sources/PgsHelperCli/main.swift
+++ PgsHelperCli/Sources/PgsHelperCli/main.swift
@@ -6,27 +6,30 @@
 // of this license document, but changing it is not allowed.
 
 import Foundation
+import ArgumentParser
 import PgsHelperDecoderSup
 
 /// I don't like standalone root level code.
 /// Let's add some useless code for feel good purpose.
 @main
-struct EntryPoint {
+struct main: ParsableCommand {
     
-    /// Application entrypoint function.
-    static func main() {
+    
+    mutating func run() throws {
         
+        // TODO See Swift Argument Parser
+        // https://swiftpackageindex.com/apple/swift-argument-parser/documentation
+        
+        // Decode PGS
         print("Reading and decoding PGS file ...")
         let segments: [AbstractSegment] = PgsDecoder.makeSegments(filepath: "/Users/yoann/Documents/3-subs.sup")
         print("Reading and decoding PGS file done !")
         print()
         
+        // Decode RLE
         print("Creating bitmaps from RLE object data ...")
         PgsDecoder.makeBitmaps(segments: segments)
         print("Creating bitmaps from RLE object data done !")
         print()
-
-        // We are done
-        exit(0)
     }
 }
