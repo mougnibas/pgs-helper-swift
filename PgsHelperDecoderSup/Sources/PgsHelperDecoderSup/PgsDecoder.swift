9046907698ffaa840ca91b83157924771571e9c5
Yoann MOUGNIBAS <yoann@mougnibas.fr>
2024-04-17 22:27:39 +0200

    checkstyle fix

--- PgsHelperDecoderSup/Sources/PgsHelperDecoderSup/PgsDecoder.swift
+++ PgsHelperDecoderSup/Sources/PgsHelperDecoderSup/PgsDecoder.swift
@@ -10,7 +10,7 @@ import CoreImage
 
 /// An utility class to decode PGS files.
 public class PgsDecoder {
-    
+
     /// Make segments from a given PGS (sup) file
     ///
     /// - Parameters:
@@ -18,14 +18,14 @@ public class PgsDecoder {
     ///
     /// - Returns : An array of segments.
     public static func makeSegments(filepath: String) -> [AbstractSegment] {
-        
+
         // Create an empty array.
         // It will grow automatically.
         // We don't know the size yet at creation time.
         var segments: [AbstractSegment] = []
 
         // Create the input stream
-        let input: InputStream = InputStream(fileAtPath: filepath)!;
+        let input: InputStream = InputStream(fileAtPath: filepath)!
 
         // Open it
         input.open()
@@ -38,62 +38,62 @@ public class PgsDecoder {
 
         // As long as there is stuff in input stream
         while input.hasBytesAvailable {
-            
+
             // Read header segment data
             buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: headerSegmentBufferSize)
             let readed: Int = input.read(buffer, maxLength: headerSegmentBufferSize)
-            
+
             // If we read 0 byte, we are done
             if (readed == 0) {
                 input.close()
                 continue
             }
-            
+
             // Decode header segment data
             let magicNumber: String               = String(bytes: [buffer[0], buffer[1]], encoding: .ascii)!
-            let pts: Int                          = Int(Utils.convert(firstByte: buffer[2], secondByte: buffer[3], thirdByte: buffer[4], lastByte: buffer[5]))
-            let dts: Int                          = Int(Utils.convert(firstByte: buffer[6], secondByte: buffer[7], thirdByte: buffer[8], lastByte: buffer[9]))
+            let pts: Int                          = Int(Utils.convert(buffer[2], buffer[3], buffer[4], buffer[5]))
+            let dts: Int                          = Int(Utils.convert(buffer[6], buffer[7], buffer[8], buffer[9]))
             let type: AbstractSegment.SegmentType = AbstractSegment.SegmentType(rawValue: buffer[10])!
             let size: Int                         = Int(Utils.convert(firstByte: buffer[11], lastByte: buffer[12]))
-            
+
             // The segment to add later to the list
             let segment: AbstractSegment
-            
+
             // Change decoding behavior depending on current segment type
             switch (type) {
-                
+
             // Is it a Presentation Composition Segment (PCS) ?
             case AbstractSegment.SegmentType.PCS :
-            
+
                 // Read PCS data
                 buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: size)
                 input.read(buffer, maxLength: size)
-                
+
                 // Decode PCS data
-                let width: Int                                                        = Int(Utils.convert(firstByte: buffer[0], lastByte: buffer[1]))
-                let height: Int                                                       = Int(Utils.convert(firstByte: buffer[2], lastByte: buffer[3]))
-                let framerate: Int                                                    = Int(buffer[4])
-                let compositionNumber: Int                                            = Int(Utils.convert(firstByte: buffer[5], lastByte: buffer[6]))
-                let compositionState: PresentationCompositionSegment.CompositionState = PresentationCompositionSegment.CompositionState(rawValue: buffer[7])!
-                let paletteUpdateFlag: Bool                                           = buffer[8] == 0x80 ? true : false
-                let paletteId: Int                                                    = Int(buffer[9])
-                let numberOfCompositionObjects: Int                                   = Int(buffer[10])
-                let objectId: Int                                                     = Int(Utils.convert(firstByte: buffer[11], lastByte: buffer[12]))
-                let windowId: Int                                                     = Int(buffer[13])
-                let objectCroppedFlag: Bool                                           = buffer[14] == 0x40 ? true : false
-                let objectHorizontalPosition: Int                                     = Int(Utils.convert(firstByte: buffer[15], lastByte: buffer[16]))
-                let objectVerticalPosition: Int                                       = Int(Utils.convert(firstByte: buffer[17], lastByte: buffer[18]))
-            
+                let width: Int                          = Int(Utils.convert(buffer[0], buffer[1]))
+                let height: Int                         = Int(Utils.convert(buffer[2], buffer[3]))
+                let framerate: Int                      = Int(buffer[4])
+                let compositionNumber: Int              = Int(Utils.convert(buffer[5], buffer[6]))
+                let compositionState: .CompositionState = .CompositionState(rawValue: buffer[7])!
+                let paletteUpdateFlag: Bool             = buffer[8] == 0x80 ? true : false
+                let paletteId: Int                      = Int(buffer[9])
+                let numberOfCompositionObjects: Int     = Int(buffer[10])
+                let objectId: Int                       = Int(Utils.convert(buffer[11], buffer[12]))
+                let windowId: Int                       = Int(buffer[13])
+                let objectCroppedFlag: Bool             = buffer[14] == 0x40 ? true : false
+                let objectHorizontalPosition: Int       = Int(Utils.convert(buffer[15], buffer[16]))
+                let objectVerticalPosition: Int         = Int(Utils.convert(buffer[17], buffer[18]))
+
                 // Create the PCS object
                 segment = PresentationCompositionSegment(magicNumber: magicNumber, pts: pts, dts: dts, type: type, size: size, width: width, height: height, framerate: framerate, compositionNumber: compositionNumber, compositionState: compositionState, paletteUpdateFlag: paletteUpdateFlag, paletteId: paletteId, numberOfCompositionObjects: numberOfCompositionObjects, objectId: objectId, windowId: windowId, objectCroppedFlag: objectCroppedFlag, objectHorizontalPosition: objectHorizontalPosition, objectVerticalPosition: objectVerticalPosition)
-                
+
             // Is it a Window Definition Segment (WDS) ?
             case AbstractSegment.SegmentType.WDS :
-                
+
                 // Read WDS data
                 buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: size)
                 input.read(buffer, maxLength: size)
-                
+
                 // Decode WDS data
                 let numberOfWindows: Int           = Int(buffer[0])
                 let windowsId: Int                 = Int(buffer[1])
@@ -101,17 +101,17 @@ public class PgsDecoder {
                 let windowsVerticalPosition: Int   = Int(Utils.convert(firstByte: buffer[4], lastByte: buffer[5]))
                 let windowWidth: Int               = Int(Utils.convert(firstByte: buffer[6], lastByte: buffer[7]))
                 let windowHeight: Int              = Int(Utils.convert(firstByte: buffer[8], lastByte: buffer[9]))
-                
+
                 // Create the WDS object
                 segment = WindowDefinitionSegment(magicNumber: magicNumber, pts: pts, dts: dts, type: type, size: size, numberOfWindows: numberOfWindows, windowsId: windowsId, windowsHorizontalPosition: windowsHorizontalPosition, windowsVerticalPosition: windowsVerticalPosition, windowWidth: windowWidth, windowHeight: windowHeight)
-                
+
             // Is it a Palette Definition Segment (PDS) ?
             case AbstractSegment.SegmentType.PDS :
-                
+
                 // Read PDS data
                 buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: size)
                 input.read(buffer, maxLength: size)
-                
+
                 // Decode PDS data
                 let paletteId: Int            = Int(buffer[0])
                 let paletteVersionNumber: Int = Int(buffer[1])
@@ -120,76 +120,76 @@ public class PgsDecoder {
                 let colorDifferenceRed: Int   = Int(buffer[4])
                 let colorDifferenceBlue: Int  = Int(buffer[5])
                 let transparency: Int         = Int(buffer[6])
-                
+
                 // Create PDS object
                 segment = PaletteDefinitionSegment(magicNumber: magicNumber, pts: pts, dts: dts, type: type, size: size, paletteId: paletteId, paletteVersionNumber: paletteVersionNumber, paletteEntryId: paletteEntryId, luminance: luminance, colorDifferenceRed: colorDifferenceRed, colorDifferenceBlue: colorDifferenceBlue, transparency: transparency)
-                
+
             // Is it an Object Definition Segment (ODS) ?
             case AbstractSegment.SegmentType.ODS :
-                
+
                 // Read ODS data
                 buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: size)
                 input.read(buffer, maxLength: size)
-                
+
                 // Decode ODS data
-                let objectId: Int                                      = Int(Utils.convert(firstByte: buffer[0], lastByte: buffer[1]))
-                let objectVersionNumber: Int                           = Int(buffer[2])
-                let sequenceFlag: ObjectDefinitionSegment.SequenceFlag = ObjectDefinitionSegment.SequenceFlag(rawValue: buffer[3])!
-                let objectDataLength: Int                              = Int(Utils.convert(firstByte: 0x00, secondByte: buffer[4], thirdByte: buffer[5], lastByte: buffer[6]))
-                let width: Int                                         = Int(Utils.convert(firstByte: buffer[7], lastByte: buffer[8]))
-                let height: Int                                        = Int(Utils.convert(firstByte: buffer[9], lastByte: buffer[10]))
+                let objectId: Int               = Int(Utils.convert(firstByte: buffer[0], lastByte: buffer[1]))
+                let objectVersionNumber: Int    = Int(buffer[2])
+                let sequenceFlag: .SequenceFlag = .SequenceFlag(rawValue: buffer[3])!
+                let objectDataLength: Int       = Int(Utils.convert(firstByte: 0x00, secondByte: buffer[4], thirdByte: buffer[5], lastByte: buffer[6]))
+                let width: Int                  = Int(Utils.convert(firstByte: buffer[7], lastByte: buffer[8]))
+                let height: Int                 = Int(Utils.convert(firstByte: buffer[9], lastByte: buffer[10]))
                 // TODO Find a better way to populate this array of bytes...
                 var pleaseMakeMeBetter: [UInt8] = []
                 for index in 11 ... size {
                     pleaseMakeMeBetter.append(buffer[index])
                 }
                 let objectData: [UInt8]                                = pleaseMakeMeBetter
-                
+
                 // Create ODS object
                 segment = ObjectDefinitionSegment(magicNumber: magicNumber, pts: pts, dts: dts, type: type, size: size, objectId: objectId, objectVersionNumber: objectVersionNumber, sequenceFlag: sequenceFlag, objectDataLength: objectDataLength, width: width, height: height, objectData: objectData)
-                
+
             // Is it and End Segment (END) ?
             case AbstractSegment.SegmentType.END :
-                
+
                 // Nothing more to read.
                 // Just create END object
                 segment = EndSegment(magicNumber: magicNumber, pts: pts, dts: dts, type: type, size: size)
             }
-            
+
             // Add the current segment to the list of segments
             segments.append(segment)
         }
-        
+
         // Return the segments
         return segments
     }
-   
+
     /// Make bitmaps from segments.
     ///
     /// - Parameters :
     ///     - segments: Segments to make bitmaps from
     public static func makeBitmaps(segments: [AbstractSegment]) {
-        
+
         // This variable will store the current subtitle identifier
         var currentId: Int = -1
-        
+
         // for each segments
         for segment in segments {
-            
+
             // PCS will store current subtitle ID
             if let pcs = segment as? PresentationCompositionSegment {
                 currentId = pcs.compositionNumber
             }
-            
+
             // Only works with ODS
             if let ods = segment as? ObjectDefinitionSegment {
-                
+
                 // Create a bitmap image from RLE object data.
                 let pixelMap: Utils.PixelMap = Utils.convert(fromRLE: ods.objectData)
-                
+
                 // Convert the pixel map to CI Image
                 let image: CIImage = Utils.convert(pixelMap: pixelMap)
-                
+
                 // TODO Remove this debug lines
                 do {
                     let destination: String = "/Users/yoann/Documents/subs/" + String(currentId) + ".png"
--- PgsHelperDecoderSup/Sources/PgsHelperDecoderSup/Utils.swift
+++ PgsHelperDecoderSup/Sources/PgsHelperDecoderSup/Utils.swift
@@ -17,7 +17,7 @@ import CoreImage
 
 /// Utility class.
 public class Utils {
-    
+
     /// Transform 4 unsigned bytes (UInt8) to a bigger UInt32 bytes
     ///
     /// - Parameters:
@@ -28,22 +28,22 @@ public class Utils {
     ///
     /// - Returns : A full UInt32 bytes
     ///
-    static func convert(firstByte: UInt8, secondByte: UInt8, thirdByte: UInt8, lastByte: UInt8) -> UInt32 {
-        
+    static func convert(_ firstByte: UInt8, _ secondByte: UInt8, _ thirdByte: UInt8, _ lastByte: UInt8) -> UInt32 {
+
         // Convert UInt8 to UInt32
-        var firstPart:  UInt32 = UInt32(firstByte)
+        var firstPart: UInt32  = UInt32(firstByte)
         var secondPart: UInt32 = UInt32(secondByte)
-        var thirdPart:  UInt32 = UInt32(thirdByte)
-        let lastPart:   UInt32 = UInt32(lastByte)
-        
+        var thirdPart: UInt32  = UInt32(thirdByte)
+        let lastPart: UInt32   = UInt32(lastByte)
+
         // Shift bytes
         firstPart  = firstPart  << 24
         secondPart = secondPart << 16
         thirdPart  = thirdPart  <<  8
-        
+
         // Finally, make a simple addition
         let fourBytesUnsignedInt: UInt32 = firstPart + secondPart + thirdPart + lastPart
-        
+
         // Return the result
         return fourBytesUnsignedInt
     }
@@ -56,22 +56,22 @@ public class Utils {
     ///
     /// - Returns : A full UInt16 bytes
     ///
-    static func convert(firstByte: UInt8, lastByte: UInt8) -> UInt16 {
-        
+    static func convert(_ firstByte: UInt8, _ lastByte: UInt8) -> UInt16 {
+
         // Convert UInt8 to UInt16
         var firstPart: UInt16 = UInt16(firstByte)
         let lastPart: UInt16  = UInt16(lastByte)
-        
+
         // Shift bytes
         firstPart = firstPart << 8
-        
+
         // Finally, make a simple addition
         let twoBytesUnsignedInt: UInt16 = firstPart + lastPart
-        
+
         // Return the result
         return twoBytesUnsignedInt
     }
-    
+
     /// Convert from RLE object data to pixel map.
     ///
     /// // See https://developer.apple.com/documentation/coregraphics/cgimage/1455149-init
@@ -81,80 +81,79 @@ public class Utils {
     ///
     /// - Returns: The decoded pixel map representation of RLE image.
     static func convert(fromRLE: [UInt8]) -> PixelMap {
-        
+
         // Index used to move
         var index: Int = 0
-        
+
         // Components of pixelmap
         var buffer: [UInt8] = []
         var tempWidth: Int  = 0
         var width: Int      = 0
         var height: Int     = 0
-        
+
         // As long as there is byte to read
-        while (index < fromRLE.count - 1) {
-            
+        while index < fromRLE.count - 1 {
+
             // Get the very first byte
             let currentByte: UInt8 = fromRLE[0 + index]
             let nextByte: UInt8
-            
+
             // If this byte is NOT 0x00, then it's "One pixel in color C".
-            if (currentByte != 0x00) {
-                
+            if currentByte != 0x00 {
+
                 // Get the pixel color
                 let color: UInt8 = currentByte
-                
+
                 // Move the index
-                index = index + 1
-                
+                index += 1
+
                 // Update width count
-                tempWidth = tempWidth + 1
-                
+                tempWidth +=1
+
                 // Add pixel to buffer
                 buffer.append(color)
                 buffer.append(color)
                 buffer.append(color)
                 buffer.append(0xFF)
-                
+
             } else {
-                
+
                 // Read next byte
                 nextByte = fromRLE[1 + index]
-                
+
                 // If this next byte is 0x00, then it's "End of line".
-                if (nextByte == 0x00) {
-                    
+                if nextByte == 0x00 {
+
                     // Move the index
-                    index = index + 2
-                    
+                    index +=2
+
                     // Update height count
-                    height = height + 1
-                    
+                    height +=1
+
                     // Reset width counters
                     width = tempWidth
                     tempWidth = 0
-                    
-                    
+
                 } else {
-                    
+
                     // Compute the discriminator
                     let discriminator = (nextByte & 0xC0) >> 6
-                    
+
                     // Act according to the discriminator
-                    switch (discriminator) {
-                        
+                    switch discriminator {
+
                     // If discriminiator is 0x00, then it's "L pixels in color 0 (L between 1 and 63)".
-                    case 0x00 :
-                        
+                    case 0x00
+
                         // We take a subpart
                         let pixels: Int = Int( nextByte & 0x3F )
-                        
+
                         // Move the index
-                        index = index + 2
-                        
+                        index += 2
+
                         // Update width count
-                        tempWidth = tempWidth + pixels
-                        
+                        tempWidth += pixels
+
                         // Add pixels to buffer
                         for index in 0 ... pixels {
                             buffer.append(0x00)
@@ -162,25 +161,25 @@ public class Utils {
                             buffer.append(0x00)
                             buffer.append(0xFF)
                         }
-                        
+
                     // If discriminiator is 0x01, then it's "L pixels in color 0 (L between 64 and 16383)".
-                    case 0x01 :
-                        
+                    case 0x01
+
                         // We take a subpart of next byte
                         let firstPart: UInt8 = nextByte & 0x3F
-                        
+
                         // We need the next next byte
                         let lastPart: UInt8 = fromRLE[2 + index]
-                        
+
                         // Convert the result
                         let pixels: Int = Int( convert(firstByte: firstPart, lastByte: lastPart) )
-                        
+
                         // Move the index
-                        index = index + 3
-                        
+                        index += 3
+
                         // Update width count
-                        tempWidth = tempWidth + pixels
-                        
+                        tempWidth += pixels
+
                         // Add pixel to buffer
                         for index in 0 ... pixels {
                             buffer.append(0x00)
@@ -188,22 +187,22 @@ public class Utils {
                             buffer.append(0x00)
                             buffer.append(0xFF)
                         }
-                        
+
                     // If discriminiator is 0x02, then it's "L pixels in color C (L between 3 and 63)".
-                    case 0x02 :
-                        
+                    case 0x02
+
                         // Compute the pixels count
                         let pixels: Int = Int( nextByte & 0x3F )
-                        
+
                         // Get color
                         let color: UInt8  = fromRLE[2 + index]
-                        
+
                         // Move the index
-                        index = index + 3
-                        
+                        index += 3
+
                         // Update width count
-                        tempWidth = tempWidth + pixels
-                        
+                        tempWidth += pixels
+
                         // Add pixel to buffer
                         for index in 0 ... pixels {
                             buffer.append(color)
@@ -211,28 +210,28 @@ public class Utils {
                             buffer.append(color)
                             buffer.append(0xFF)
                         }
-                        
+
                     // If discriminiator is 0x03, then it's "L pixels in color C (L between 64 and 16383)".
-                    case 0x03 :
-                        
+                    case 0x03
+
                         // We take a subpart of next byte
                         let firstPart: UInt8 = nextByte & 0x3F
-                        
+
                         // We need the next next byte
                         let lastPart: UInt8 = fromRLE[2 + index]
-                        
+
                         //  Convert the pixels count
                         let pixels: Int = Int( convert(firstByte: firstPart, lastByte: lastPart) )
-                        
+
                         // Get color
                         let color: UInt8  = fromRLE[3 + index]
-                        
+
                         // Move the index
-                        index = index + 4
-                        
+                        index += 4
+
                         // Update width count
-                        tempWidth = tempWidth + pixels
-                        
+                        tempWidth += pixels
+
                         // Add pixel to buffer
                         for index in 0 ... pixels {
                             buffer.append(color)
@@ -240,22 +239,22 @@ public class Utils {
                             buffer.append(color)
                             buffer.append(0xFF)
                         }
-                        
-                    default :
+
+                    default
                         print("We shouldn't be here")
                         exit(-1)
                     }
                 }
             }
         }
-        
+
         // Create the PixelMap
-        let pixelMap : PixelMap = PixelMap(width: width, height: height, buffer: buffer)
-        
+        let pixelMap: PixelMap = PixelMap(width: width, height: height, buffer: buffer)
+
         // Return the PixelMap
         return pixelMap
     }
-    
+
     /// Convert from bitmap pixel map to CoreImage Image.
     ///
     /// - Parameters :
@@ -263,24 +262,24 @@ public class Utils {
     ///
     /// - Returns : An image in CoreImage format.
     static func convert(pixelMap: PixelMap) -> CIImage {
-        
+
         // Elements of CI Image
         let bitmapData: Data = Data(pixelMap.buffer)
         let bytesPerRow: Int = pixelMap.width * 4
         let size: CGSize = CGSize(width: pixelMap.width, height: pixelMap.height)
         let colorSpace: CGColorSpace = CGColorSpaceCreateDeviceRGB()
-        
+
         // Create the CI Image
         let ciImage = CIImage(bitmapData: bitmapData,
                               bytesPerRow: bytesPerRow,
                               size: size,
                               format: CIFormat.RGBA8,
                               colorSpace: colorSpace)
-        
+
         // Return the CI Image
         return ciImage
     }
-    
+
     /// Convert from bitmap pixel map to CoreGraphics Image.
     ///
     /// - Parameters :
@@ -288,20 +287,20 @@ public class Utils {
     ///
     /// - Returns : An image in CoreGraphics format.
     static func convert(pixelMap: PixelMap) -> CGImage {
-        
+
         // Create a CI Image
         let image: CIImage = convert(pixelMap: pixelMap)
-        
+
         // Create a default context
         let context: CIContext = CIContext()
-        
+
         // Create a CGImage from CIImage
         let cgimage: CGImage = context.createCGImage(image, from: image.extent)!
-        
+
         // Return the CGImage
         return cgimage
     }
-    
+
     /// Write a CIImage to a destination file, in PNG format.
     ///
     /// - Parameters :
@@ -310,29 +309,29 @@ public class Utils {
     ///
     /// - Throws : An error if we can't write the file.
     static func write(image: CIImage, destination: String) throws {
-        
+
         // Create a default context
         let context: CIContext = CIContext()
-        
+
         // Create a PNG representation of the image
         let format: CIFormat = CIFormat.RGBA8
         let colorSpace: CGColorSpace = image.colorSpace!
         let imageData = context.pngRepresentation(of: image, format: format, colorSpace: colorSpace)!
-        
+
         // Try to write the file
         let url: URL = URL(fileURLWithPath: destination)
         try imageData.write(to: url)
     }
-    
+
     /// A pixel map.
     public struct PixelMap {
-        
+
         /// Width in pixels.
         let width: Int
-        
+
         /// Height in pixels.
         let height: Int
-        
+
         /// A RGBA buffer (Red, Green, Blue, Alpha components). One byte per component.
         let buffer: [UInt8]
     }
