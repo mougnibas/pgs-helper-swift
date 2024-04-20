6a4c8710c8abae8fda111ac9b8b200ac5a4b1559
Yoann MOUGNIBAS <yoann@mougnibas.fr>
2024-04-20 16:49:53 +0200

    Something is actually decoded. Amazing.

--- PgsHelperBitmapToText/Sources/PgsHelperBitmapToText/BitmapToText.swift
+++ PgsHelperBitmapToText/Sources/PgsHelperBitmapToText/BitmapToText.swift
@@ -6,6 +6,8 @@
 // of this license document, but changing it is not allowed.
 
 import Foundation
+import CoreImage
+import Vision
 
 import PgsHelperCommon
 
@@ -17,6 +19,36 @@ public class BitmapToText {
     
     public static func recognizeText(image: PixmapPicture) -> String {
         
+        // Get the CGImage on which to perform requests.
+        let cgImage: CGImage = Utils.convert(image)
+        
+        // Create a new image-request handler.
+        let requestHandler = VNImageRequestHandler(cgImage: cgImage)
+        
+        // Create a new request to recognize text.
+        let request = VNRecognizeTextRequest(completionHandler: recognizeTextHandler)
+
+        do {
+            // Perform the text-recognition request.
+            try requestHandler.perform([request])
+        } catch {
+            print("Unable to perform the requests: \(error).")
+        }
+        
         return "TODO"
+    }
+    
+    static func recognizeTextHandler(request: VNRequest, error: Error?) {
+        
+        guard let observations =
+                request.results as? [VNRecognizedTextObservation] else {
+            return
+        }
+        let recognizedStrings = observations.compactMap { observation in
+            // Return the string of the top VNRecognizedText instance.
+            return observation.topCandidates(1).first?.string
+        }
+        
+        print("omg : '\(recognizedStrings)'");
     }
 }
